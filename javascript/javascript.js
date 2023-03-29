d857c9cde4678767c2744e9fa6bfed39a365838f
ingyukim <ikim59@bcit.ca>
2023-03-28 17:09:24 -0700

    Add  function cancel()

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,18 +1,31 @@
-let dark_mode = 0;
+let darkmode_switch = 0;
 function DarkMode() {
     var body_element = document.body;
     var side_bar = document.querySelector("header");
-    var darkmode_button = document.querySelector("#darkmode_button");
-    if (dark_mode === 0) {
+    var darkmode_button = document.getElementById("darkmode_button");
+    if (darkmode_switch === 0) {
         body_element.classList.toggle("dark_mode_body");
         side_bar.classList.toggle("dark_mode_sidebar");
-        darkmode_button.textContent = "Light Mode"
-        return dark_mode = 1;
+        darkmode_button.textContent = "Light Mode";
+        return darkmode_switch = 1;
     }
     else {
         body_element.classList.remove("dark_mode_body");
         side_bar.classList.remove("dark_mode_sidebar");
-        darkmode_button.textContent = "Dark Mode"
-        return dark_mode = 0;
+        darkmode_button.textContent = "Dark Mode";
+        return darkmode_switch = 0;
     }
+};
+
+let cancel_switch = 0;
+let newnote_switch = 0;
+
+function cancel() {
+    var text_area = document.getElementById("take_note");
+    var save_button = document.getElementById("save_button");
+    var cancel_button = document.getElementById("cancel_button");
+    text_area.classList.toggle("hiding");
+    save_button.classList.toggle("hiding");
+    cancel_button.classList.toggle("hiding");
+    return cancel_switch = 1;
 };
